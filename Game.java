aa16eaadd803674340b21a1fd1585f8337c19ed1
Andrew Esteban <andrewesteban1999@gmail.com>
2019-09-26 20:34:07 +1000

    game playable - no checks or end

--- Game.java
+++ Game.java
@@ -7,6 +7,11 @@ public class Game {
     private Multiplayer player;
     private Scanner sc;
 
+    private int[][] board;
+
+    private boolean first;
+    private boolean host;
+
     public Game() throws Exception {
         this.sc = new Scanner(System.in);
 
@@ -18,20 +23,123 @@ public class Game {
             System.out.print("What port is the host on? ");
             int port = this.sc.nextInt();
             this.player = new Client(port);
+            this.first = false;
+            this.host = false;
 
         } else {
 
             System.out.print("What port do you want to host on? ");
             int port = this.sc.nextInt();
             this.player = new Host(port);
+            this.first = true;
+            this.host = true;
+
         }
+
+        this.board = new int[3][3];
     }
 
-    public boolean play() {
-        return true;
+    public void makeMove() throws Exception {
+
+        // Game is over
+        if (this.checkGame() != 0) {
+            int[] move = {-1, -1};
+            this.player.writeArray(move);
+            return;
+        }
+
+        System.out.print("\nEnter coordinates: ");
+
+        // TODO: Don't allow invalid coordinates
+        int x = this.sc.nextInt();
+        int y = this.sc.nextInt();
+
+        if (this.host) {
+            this.board[x][y] = 1;
+        } else {
+            this.board[x][y] = 2;
+        }
+
+        int[] coordinates = {x, y};
+        this.player.writeArray(coordinates);
+
     }
 
-    public void end() {
+    public void waitMove() throws Exception {
+
+        int[] coordinates = this.player.readArray();
+
+        if (coordinates[0] == -1 && coordinates[1] == -1) {
+            return;
+        }
+
+        if (this.host) {
+            this.board[coordinates[0]][coordinates[1]] = 2;
+        } else {
+            this.board[coordinates[0]][coordinates[1]] = 1;
+        }
+
+    }
+
+    public int checkGame() throws Exception {
+
+        return 0;
+
+    }
+
+    public void printBoard() {
+
+        System.out.println("");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == 0) {
+                    System.out.print(" ");
+                } else if (board[i][j] == 1) {
+                    System.out.print("X");
+                } else {
+                    System.out.print("O");
+                }
+
+                if (j == 0 || j == 1) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+
+            if (i == 0 || i == 1) {
+                System.out.println("-----");
+            }
+        }
+
+    }
+
+    public boolean play() throws Exception {
+
+        if (this.first) {
+            this.printBoard();
+            this.makeMove();
+            this.printBoard();
+            this.waitMove();
+        } else {
+            this.waitMove();
+            this.printBoard();
+            this.makeMove();
+            this.printBoard();
+        }
+
+        boolean result = true;
+
+        if (this.checkGame() != 0) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public void end() throws Exception {
+
+        this.player.close();
 
     }
 
@@ -45,5 +153,6 @@ public class Game {
         }
 
         g.end();
+        
     }
 }
