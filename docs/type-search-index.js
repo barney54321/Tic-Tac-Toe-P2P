a7aaf535c9d1e0ea3d0c4d67ea1df6a6a41dcc1b
Andrew Esteban <andrewesteban1999@gmail.com>
2019-09-27 09:40:37 +1000

    created javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"Host"},{"p":"<Unnamed>","l":"Multiplayer"}]
