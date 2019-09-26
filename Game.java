9779982fdd9df770122bda49a7b40045eb701bdc
Andrew Esteban <andrewesteban1999@gmail.com>
2019-09-27 08:27:55 +1000

    added comments to Game

--- Game.java
+++ Game.java
@@ -4,15 +4,16 @@ import java.net.*;
 
 public class Game {
 
-    private Multiplayer player;
-    private Scanner sc;
+    private Multiplayer player; // Manages Socket and the streams
+    private Scanner sc; // Reads user input
 
-    private int[][] board;
+    private int[][] board; // Stores the board state
 
-    private boolean first;
-    private boolean host;
+    private boolean first; // Does the player move first
+    private boolean host; // Is the player hosting
 
     public Game() throws Exception {
+
         this.sc = new Scanner(System.in);
 
         System.out.print("Do you want to join or host? ");
@@ -20,6 +21,7 @@ public class Game {
 
         if (response.toLowerCase().equals("join")) {
 
+            // Join to the host Socket
             System.out.print("What port is the host on? ");
             int port = this.sc.nextInt();
             this.player = new Client(port);
@@ -28,6 +30,7 @@ public class Game {
 
         } else {
 
+            // Set up host Socket
             System.out.print("What port do you want to host on? ");
             int port = this.sc.nextInt();
             this.player = new Host(port);
@@ -41,7 +44,7 @@ public class Game {
 
     public void makeMove() throws Exception {
 
-        // Game is over
+        // Check if game is over
         if (this.checkGame() != 0) {
             int[] move = {-1, -1};
             this.player.writeArray(move);
@@ -53,6 +56,7 @@ public class Game {
         int x = this.sc.nextInt();
         int y = this.sc.nextInt();
 
+        // While coordinates are invalid, force new coordinate values
         if (x < 0 || x > 2 || y < 0 || y > 2 || this.board[x][y] != 0) {
             while (x < 0 || x > 2 || y < 0 || y > 2 || this.board[x][y] != 0) {
                 System.out.println("\nINVALID COORDINATES\n");
@@ -62,12 +66,14 @@ public class Game {
             }
         }
 
+        // Place the piece on the board
         if (this.host) {
             this.board[x][y] = 1;
         } else {
             this.board[x][y] = 2;
         }
 
+        // Send coordinates to other player
         int[] coordinates = {x, y};
         this.player.writeArray(coordinates);
 
@@ -75,12 +81,15 @@ public class Game {
 
     public void waitMove() throws Exception {
 
+        // Block until coordinates are received
         int[] coordinates = this.player.readArray();
 
+        // Game is over if invalid coordinates
         if (coordinates[0] == -1 && coordinates[1] == -1) {
             return;
         }
 
+        // Place opponent's piece
         if (this.host) {
             this.board[coordinates[0]][coordinates[1]] = 2;
         } else {
@@ -93,25 +102,31 @@ public class Game {
 
         // Check horizontals
         for (int i = 0; i < 3; i++) {
-            if (this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2] && this.board[i][0] != 0) {
-                return this.board[i][0];
+            if (this.board[i][0] != 0) {
+                if (this.board[i][0] == this.board[i][1] && this.board[i][0] == this.board[i][2]) {
+                    return this.board[i][0];
+                }
             }
         }
 
         // Check verticals
         for (int i = 0; i < 3; i++) {
-            if (this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i] && this.board[0][i] != 0) {
-                return this.board[0][i];
+            if (this.board[0][i] != 0) {
+                if (this.board[0][i] == this.board[1][i] && this.board[0][i] == this.board[2][i]) {
+                    return this.board[0][i];
+                }
             }
         }
 
         // Check diagonals
-        if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2] && this.board[0][0] != 0) {
-            return this.board[0][0];
-        }
+        if (this.board[1][1] != 0) {
+            if (this.board[0][0] == this.board[1][1] && this.board[0][0] == this.board[2][2]) {
+                return this.board[0][0];
+            }
 
-        if (this.board[2][0] == this.board[1][1] && this.board[2][0] == this.board[0][2] && this.board[0][2] != 0) {
-            return this.board[0][2];
+            if (this.board[2][0] == this.board[1][1] && this.board[2][0] == this.board[0][2]) {
+                return this.board[0][2];
+            }
         }
 
         // Check for draw
@@ -200,15 +215,17 @@ public class Game {
 
     public static void main(String[] args) throws Exception {
 
+        // Create new Game object
         Game g = new Game();
 
+        // Continue to make moves until result is achieved
         boolean play = g.play();
         while (play) {
             play = g.play();
         }
 
+        // Print result and close Sockets and streams
         g.printResult();
-
         g.end();
 
     }
